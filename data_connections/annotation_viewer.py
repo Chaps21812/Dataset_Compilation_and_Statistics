Corrected prominence, SNR, and added a few other signal metrics. These involve local calculation of max mean and median, and were previously incorrect because of the lack of median subtraction and stuff.

data_connections/annotation_viewer.py
@@ -38,8 +38,8 @@ def plot_annotation_subset(pandas_library:pd.DataFrame, dataset_path:str, view_s
         raw_data = fits_file[0].data
 
         #The XY coordinates are reverse intentionally. Beware!
-        x_res = hdu["NAXIS2"]
-        y_res = hdu["NAXIS1"]
+        y_res = hdu["NAXIS2"]
+        x_res = hdu["NAXIS1"]
 
         noise = np.std(raw_data)
         median_pixel = np.median(raw_data)
@@ -50,17 +50,18 @@ def plot_annotation_subset(pandas_library:pd.DataFrame, dataset_path:str, view_s
             x_cord= object["x_center"]*x_res
             y_cord= object["y_center"]*y_res
 
-            half = 100 // 2
-            y_start = max(0, y_cord - half)
-            y_end   = min(data.shape[0], y_cord + half)
+            half = 50
             x_start = max(0, x_cord - half)
             x_end   = min(data.shape[1], x_cord + half)
+            y_start = max(0, y_cord - half)
+            y_end   = min(data.shape[0], y_cord + half)
 
-            window = raw_data[int(x_start):int(x_end), int(y_start):int(y_end)]
+            window = raw_data[int(y_start):int(y_end), int(x_start):int(x_end)]
             minimum = np.min(window)
-
-            signal = raw_data[int(x_cord), int(y_cord)]
+            signal = raw_data[int(y_cord), int(x_cord)]
             snr_tuple = (object["x_center"]*x_res, object["y_center"]*y_res,"Prom: {}".format(signal/minimum))
+
+
 
             if object['class_name']=="Satellite": 
                 if view_satellite: plot_image_with_bbox(data,object['x_center']*x_res,object['y_center']*y_res,object['x_max']*x_res-object['x_min']*x_res,index, json_path, dpi=500, snr=snr_tuple)

data_connections/collect_stats.py
@@ -2,6 +2,7 @@ from astropy.io import fits
 from datetime import datetime
 import numpy as np
 from math import atan2
+from plots import plot_image_with_line, z_scale_image
 
 
 def directed_circular_stats(angles_deg:list) -> tuple[float, float, float]:
@@ -65,7 +66,7 @@ def circular_stats(angles_deg:list) -> tuple[float, float, float]:
 
     return mean_angle_deg, circular_std, R
 
-def collect_stats(json_content:dict, fits_content:fits, padding:int=30) -> tuple[dict,dict]:
+def collect_stats(json_content:dict, fits_content:fits, padding:int=20) -> tuple[dict,dict]:
     sample_attributes = {}
     object_attributes = []
     padding = padding
@@ -73,8 +74,8 @@ def collect_stats(json_content:dict, fits_content:fits, padding:int=30) -> tuple
     hdu = fits_content[0].header
     data = fits_content[0].data
 
-    x_res = hdu["NAXIS2"]
-    y_res = hdu["NAXIS1"]
+    x_res = hdu["NAXIS1"]
+    y_res = hdu["NAXIS2"]
 
     sample_attributes["filename"] = json_content["file"]["filename"]
     sample_attributes["id_sensor"] = json_content["file"]["id_sensor"]
@@ -105,9 +106,8 @@ def collect_stats(json_content:dict, fits_content:fits, padding:int=30) -> tuple
 
         x_cord= object["x_center"]*x_res
         y_cord= object["y_center"]*y_res
+        signal = data[int(y_cord), int(x_cord)]
 
-        detection_dict["measured_snr"] = data[int(x_cord),int(y_cord)]/sample_attributes["std_intensity"]
-        detection_dict["measured_intensity_over_median"] = data[int(x_cord),int(y_cord)]/sample_attributes["median_intensity"]
         if object['class_name']=="Satellite": 
             sats+=1
             detection_dict["filename"] = json_content["file"]["filename"]
@@ -127,18 +127,6 @@ def collect_stats(json_content:dict, fits_content:fits, padding:int=30) -> tuple
             x_min = min(detection_dict["x_min"], detection_dict["x_max"])*x_res
             y_max = max(detection_dict["y_min"], detection_dict["y_max"])*y_res
             y_min = min(detection_dict["y_min"], detection_dict["y_max"])*y_res
-
-            y_start = max(0, y_min - padding)
-            y_end   = min(data.shape[0], y_max + padding)
-            x_start = max(0, x_min - padding)
-            x_end   = min(data.shape[1], x_max + padding)
-
-            window = data[int(x_start):int(x_end), int(y_start):int(y_end),]
-            minimum = np.min(window)
-            signal = data[int(x_cord), int(y_cord)]
-
-            detection_dict["prominence"] = signal/minimum
-
 
         if object['class_name']=="Star": 
             stars+=1
@@ -160,19 +148,25 @@ def collect_stats(json_content:dict, fits_content:fits, padding:int=30) -> tuple
             y_max = max(detection_dict["y1"], detection_dict["y2"])*y_res
             y_min = min(detection_dict["y1"], detection_dict["y2"])*y_res
 
-            y_start = max(0, y_min - padding)
-            y_end   = min(data.shape[0], y_max + padding)
-            x_start = max(0, x_min - padding)
-            x_end   = min(data.shape[1], x_max + padding)
-
-            window = data[int(x_start):int(x_end), int(y_start):int(y_end),]
-            minimum = np.min(window)
-            signal = data[int(x_cord), int(y_cord)]
-
-            detection_dict["prominence"] = signal/minimum
-            
             streak_angles.append(detection_dict["angle"])
-            streak_lengths.append(detection_dict["length"])
+            streak_lengths.append(detection_dict["length"])    
+
+        y_start = max(0, y_min - padding)
+        y_end   = min(data.shape[0], y_max + padding)
+        x_start = max(0, x_min - padding)
+        x_end   = min(data.shape[1], x_max + padding)
+
+        window = data[int(y_start):int(y_end),int(x_start):int(x_end)]
+        local_minimum = np.min(window)
+        local_median = np.median(window)
+        local_maximum = np.max(window)
+        local_std = np.std(window)
+        
+        detection_dict["local_prominence"] = (signal-local_minimum)/(local_std+1*10**-5)
+        detection_dict["local_snr"] = (signal-local_median)/(local_std+1*10**-5)
+        detection_dict["max_signal_diff"] = (local_maximum-signal)/(local_maximum-local_minimum+1*10**-5)
+        detection_dict["global_snr"] = (signal-local_median)/sample_attributes["std_intensity"]
+        
         object_attributes.append(detection_dict)
 
     if len(streak_angles) > 0: 

data_connections/plots.py
@@ -294,7 +294,7 @@ def plot_time_column(series: pd.Series, bins: int = 30, filepath: str=None, dpi:
         # print(f"Plot saved to: {full_path}")
     plt.show()
 
-def plot_image_with_bbox(image: np.ndarray, x: int, y: int, size: int, object_n:int, full_file_path:str=None, dpi: int = 300, snr:tuple=None, alpha=.3):
+def plot_image_with_bbox(image: np.ndarray, x: int, y: int, size: int, object_n:int, full_file_path:str=None, dpi: int = 300, snr:tuple=None, alpha=.3, show:bool=False):
     """
     Plots an image with a square annotation and a padding of 100 pixels on all sides.
 
@@ -348,10 +348,11 @@ def plot_image_with_bbox(image: np.ndarray, x: int, y: int, size: int, object_n:
         # Save the plot
         plt.tight_layout()
         plt.savefig(full_path, dpi=dpi, bbox_inches='tight')
+    if show: plt.show()
     plt.close()
         # print(f"Plot saved to: {full_path}")
 
-def plot_image_with_line(image: np.ndarray, x1: int, y1: int, x2: int, y2: int, object_n:int, full_file_path:str=None, dpi: int = 300, snr:tuple=None, alpha=.3):
+def plot_image_with_line(image: np.ndarray, x1: int, y1: int, x2: int, y2: int, object_n:int, full_file_path:str=None, dpi: int = 300, snr:tuple=None, alpha=.3, show:bool=False):
     """
     Plots an image with a line (x1, y1) -> (x2, y2) and a 100-pixel padded bounding box around the line.
 
@@ -408,6 +409,7 @@ def plot_image_with_line(image: np.ndarray, x1: int, y1: int, x2: int, y2: int, 
         plt.tight_layout()
         plt.savefig(full_path, dpi=dpi, bbox_inches='tight')
         # print(f"Plot saved to: {full_path}")
+    if show: plt.show()
     plt.close()
 
 def plot_image(image: np.ndarray, full_file_path:str=None, dpi: int = 300):